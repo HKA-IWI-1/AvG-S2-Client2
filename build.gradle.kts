ed7e7d630317012db93e8a576714b3b0ab5d8cea
flomanuel <flomanuel.github@gmail.com>
2024-04-15 15:56:55 +0200

    Update gradle bootRun task setting to enable DEV profile

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.4"
 }
 
+val activeProfiles = "dev"
 group = "HKA-IWI-1"
 version = "0.0.1-alpha"
 
@@ -73,4 +74,9 @@ tasks.named(
             """.trimMargin("|")
         )
     }
+}
+
+tasks.named("bootRun", org.springframework.boot.gradle.tasks.run.BootRun::class.java) {
+    systemProperty("spring.profiles.active", activeProfiles)
+    systemProperty("logging.file.name", "./build/log/application.log")
 }
